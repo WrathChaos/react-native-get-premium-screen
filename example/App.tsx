e349e171b5aea2ef129f0f10a005c05d178988c6
FreakyCoder <kurayogun@gmail.com>
2021-11-18 00:34:05 +0300

    ref: README is updated

--- example/App.tsx
+++ example/App.tsx
@@ -1,16 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from "react";
 import {} from "react-native";
-import PremiumScreen, { IPlan } from "./build/dist/PremiumScreen";
+import PremiumScreen, { IPlan } from "react-native-get-premium-screen";
 
 const descriptionText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam massa mauris, pretium bibendum orci in, consectetur vulputate erat.";
